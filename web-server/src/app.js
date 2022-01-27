bf736f0db296fdd324483cbfe5cabc7a3e0f8ba2
BlueWorldInc <keamybakunin@gmail.com>
2022-01-27 19:20:03 +0100

    save

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,9 +7,32 @@ const path_file_help = path.join(path_dir_public, "./help.html");
 
 const app = express()
 
+app.set("view engine", "hbs")
+
 app.use(express.static(path_dir_public))
 // app.use("/about", express.static(path_file_about))
 // app.use("/help", express.static(path_file_help))
+
+app.get("", (req, res) => {
+	res.render("index", {
+		title: 'Weather appp',
+		name: 'Hety'
+	})
+})
+
+app.get("/help", (req, res) => {
+	res.render("help", {
+		title: 'Help appp',
+		name: 'Hety'
+	})
+})
+
+app.get("/about", (req, res) => {
+	res.render("about", {
+		title: 'about appp',
+		name: 'Hety'
+	})
+})
 
 app.get("/weather", (req, res) => {
 	res.send({
