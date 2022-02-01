f55b579771204a72c367aa00edad4393148140ae
BlueWorldInc <keamybakunin@gmail.com>
2022-02-01 17:15:12 +0100

    save

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -4,8 +4,6 @@ const {MongoClient, ObjectId} = require('mongodb');
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 const id = new ObjectId();
-console.log(id);
-console.log(id.getTimestamp());
 
 MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
 	if (error) {
@@ -13,44 +11,11 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
 	}
 	const db = client.db(databaseName);
 
-	db.collection('users').insertOne({
-		_id: id,
-		name: 'Andrew',
-		age: 27
-	}, (error, result) => {
+	db.collection("users").findOne({_id: new ObjectId("61f95b4e8da72d5ff0f11a37")}, (error, user) => {
 		if (error) {
-			return console.log("A problem appeared when inserting");
+			return console.log("Unable to fetch the user");
 		}
-		console.log(result.ops);
-	});
-
-	// db.collection('users').insertMany([{
-	// 	name: "Jen",
-	// 	age: 29
-	// }, {
-	// 	name: "Gunthar",
-	// 	age: 45
-	// }], (error, result) => {
-	// 	if (error) {
-	// 		return console.log("A problem appeared when inserting");
-	// 	}
-	// 	console.log(result.ops);
-	// })
-
-	// db.collection('tasks').insertMany([{
-	// 	description: "lorem",
-	// 	completed: true
-	// }, {
-	// 	description: "lorem",
-	// 	completed: true
-	// }, {
-	// 	description: "lorem",
-	// 	completed: false
-	// }], (error, result) => {
-	// 	if (error) {
-	// 		return console.log("A problem appeared when inserting");
-	// 	}
-	// 	console.log(result.ops);
-	// })
+		console.log(user);
+	})
 
 });
