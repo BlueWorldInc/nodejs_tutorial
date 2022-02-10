db4ce4d028f90800ac0e5e3f057fadf13d60afa8
BlueWorldInc <keamybakunin@gmail.com>
2022-02-10 18:47:01 +0100

    save

--- task-manager/index.js
+++ task-manager/index.js
@@ -48,9 +48,25 @@ app.post('/tasks', (req, res) => {
 	});
 });
 
-// app.get('/', (req, res) => {
-// 	res.send("testing");
-// });
+app.get('/tasks', (req, res) => {
+	Task.find({}).then((tasks) => {
+		res.status(200).send(tasks)
+	}).catch((e) => {
+		res.status(500).send(e);
+	});
+});
+
+app.get('/tasks/:id', (req, res) => {
+	const _id = req.params.id;
+	Task.findById(_id).then((task) => {
+		if (!task) {
+			return res.status(404).send();
+		}
+		res.send(task)
+	}).catch((e) => {
+		res.status(500).send(e);
+	});
+});
 
 app.listen(port, () => {
 	console.log("Server is up on port");
