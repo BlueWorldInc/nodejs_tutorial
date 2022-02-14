de56265ee02e32889509aa7b4dae6cfef00ade0e
BlueWorldInc <keamybakunin@gmail.com>
2022-02-14 19:03:30 +0100

    save

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,10 +1,22 @@
 require("../src/db/mongoose");
 const User = require("../src/models/user");
 
-User.findByIdAndUpdate("62040b91fbdfb836d41f72e5", { age: 6 }).then((user) => {
-	console.log(user);
-	return User.countDocuments({age: 1});
-}).then((result) => {
+// User.findByIdAndUpdate("62040b91fbdfb836d41f72e5", { age: 6 }).then((user) => {
+// 	console.log(user);
+// 	return User.countDocuments({age: 1});
+// }).then((result) => {
+// 	console.log(result);
+// }).catch((e) => {
+// 	console.log(e);
+// });
+
+const updateUserAgeAndCount = async (id, age) => {
+	const user = await User.findByIdAndUpdate(id, {age});
+	const count = await User.countDocuments({age});
+	return count;
+}
+
+updateUserAgeAndCount("62040b91fbdfb836d41f72e5", 8).then((result) => {
 	console.log(result);
 }).catch((e) => {
 	console.log(e);
--- task-manager/playground/promise-chaining2.js
+++ task-manager/playground/promise-chaining2.js
@@ -1,11 +1,22 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/task");
 
-Task.findByIdAndDelete("6203e8a0aee8e40eac3564be").then((deletedTask) => {
-	console.log(deletedTask);
+// Task.findByIdAndDelete("6203e8a0aee8e40eac3564be").then((deletedTask) => {
+// 	console.log(deletedTask);
+// 	return Task.countDocuments({completed: false});
+// }).then((incompletTaskCount) => {
+// 	console.log(incompletTaskCount);
+// }).catch((e) => {
+// 	console.log(e);
+// });
+
+const deleteTaskAndCountIncomplete = async (id) => {
+	await Task.findByIdAndDelete(id);
 	return Task.countDocuments({completed: false});
-}).then((incompletTaskCount) => {
-	console.log(incompletTaskCount);
+}
+
+deleteTaskAndCountIncomplete("62040d4b6a7f824be8298504").then((result) => {
+	console.log(result);
 }).catch((e) => {
-	console.log(e);
-});
+	console.log("e", e);
+})
