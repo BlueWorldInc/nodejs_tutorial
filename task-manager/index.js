34ff5464d58401c122b13d57c4db45653ad2d80c
BlueWorldInc <keamybakunin@gmail.com>
2022-02-16 11:47:53 +0100

    save

--- task-manager/index.js
+++ task-manager/index.js
@@ -14,3 +14,19 @@ app.listen(port, () => {
 	console.log("Server is up on port");
 });
 
+const bcrypt = require("bcryptjs");
+
+const myFunction = async () => {
+	const password = "Red12345!";
+	const hashedPassword = await bcrypt.hash(password, 8);
+	const hashedPassword2 = await bcrypt.hash(password, 8);
+
+	console.log(password);
+	console.log(hashedPassword);
+	console.log(hashedPassword2);
+
+	const isMatch = await bcrypt.compare("Red12345!", hashedPassword);
+	console.log(isMatch);
+}
+
+myFunction();
