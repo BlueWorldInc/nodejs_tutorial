1aafcd73976c2c070946b7be8caf2a61a3bec37c
BlueWorldInc <keamybakunin@gmail.com>
2022-02-16 15:41:48 +0100

    save

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,11 +1,9 @@
 const User = require("../models/user.js");
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
 
 router.post('/users', async (req, res) => {
 	const user = new User(req.body);
-
 	try {
 		await user.save();
 		res.status(201).send(user);
