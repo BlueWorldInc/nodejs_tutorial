5c0aec34a608b4df0103aa53ddb8129b002132a4
BlueWorldInc <keamybakunin@gmail.com>
2022-02-17 15:45:04 +0100

    save

--- task-manager/index.js
+++ task-manager/index.js
@@ -2,9 +2,26 @@ const express = require("express");
 require("./src/db/mongoose.js");
 const routerUser = require("./src/routers/user.js");
 const routerTask = require("./src/routers/task.js");
+const routerTask = require("./src/middleware/auth.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+// Permet d'ajouter un middleware qui s'éxécute entre une request (de l'utilisateur) et une route
+// app.use((req, res, next) => {
+// 	if (req.method === "GET") {
+// 		res.send("GET requests are disabled");
+// 	} else {
+// 		next();
+// 	}
+// });
+
+// app.use((req, res, next) => {
+// 	res.status(503).send("The site is under maintenance");
+// });
+
+
 
 app.use(express.json());
 app.use(routerUser);
