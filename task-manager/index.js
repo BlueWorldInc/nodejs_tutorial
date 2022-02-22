3e39974b507187f07a40a7bae702e212f2276a32
BlueWorldInc <keamybakunin@gmail.com>
2022-02-22 19:07:52 +0100

    save

--- task-manager/index.js
+++ task-manager/index.js
@@ -27,3 +27,20 @@ app.listen(port, () => {
 // };
 
 // console.log(JSON.stringify(pet));
+
+// Test ref
+
+const Task = require("./src/models/task.js");
+const User = require("./src/models/user.js");
+
+const main = async () => {
+	const task = await Task.findById('6215252876d8eb55e444f588');
+	await task.populate('owner').execPopulate();
+	console.log(task.owner);
+
+	const user = await User.findById('621523f75d53c31448eb4cf2');
+	await user.populate('tasks').execPopulate();
+	console.log(user.tasks);
+};
+
+main();
--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -4,7 +4,11 @@ const router = express.Router();
 const auth = require("../middleware/auth.js");
 
 router.post('/tasks', auth, async (req, res) => {
-	const task = new Task(req.body);
+	const task = new Task({
+		...req.body,
+		owner: req.user._id
+	});
+
 	try {
 		await task.save();
 		res.status(201).send(task);
