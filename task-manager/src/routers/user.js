3932a2fe55c1d26b14818d257955ff012d89e2cf
BlueWorldInc <keamybakunin@gmail.com>
2022-02-22 11:13:40 +0100

    save

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -28,7 +28,7 @@ router.post('/users/logout', auth, async (req, res) => {
 	try {
 		req.user.tokens = req.user.tokens.filter((token) => {
 			return (token.token !== req.token);	// <-- si le token qui est fournit en post par l'utilisateur est égale à un 
-			// ^-- token dans la liste on le vire.
+												// ^-- token dans la liste on le vire.
 		});
 		await req.user.save();
 		res.status(200).send();
@@ -49,29 +49,9 @@ router.post('/users/logoutAll', auth, async (req, res) => {
 
 router.get('/users/me', auth, async (req, res) => {
 	res.send(req.user);
-	// try {
-	// 	const users = await User.find({});
-	// 	res.status(200).send(users)
-	// } catch (e) {
-	// 	res.status(500).send(e);
-	// }
 });
 
-router.get('/users/:id', async (req, res) => {
-	const _id = req.params.id;
-	try {
-		const user = await User.findById(_id);
-		if (!user) {
-			return res.status(404).send();
-		}
-		res.send(user);
-	} catch (e) {
-		res.status(500).send(e);
-	}
-});
-
-router.patch('/users/:id', async (req, res) => {
-	const _id = req.params.id;
+router.patch('/users/me', auth, async (req, res) => {
 	const updates = Object.keys(req.body);
 	const allowedUpdates = ["name", "email", "age", "password"];
 	const isValidOperation = updates.every((update) => {
@@ -83,27 +63,22 @@ router.patch('/users/:id', async (req, res) => {
 	}
 
 	try {
-		const user = await User.findById(_id);
-		updates.forEach((update) => user[update] = req.body[update]);
-		await user.save();
-		// const user = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
-		if (!user) {
+
+		updates.forEach((update) => req.user[update] = req.body[update]);
+		await req.user.save();
+		if (!req.user) {
 			return res.status(404).send();
 		}
-		res.send(user);
+		res.send(req.user);
 	} catch (e) {
 		res.status(400).send(e);
 	}
 });
 
-router.delete('/users/:id', async (req, res) => {
-	const _id = req.params.id;
+router.delete('/users/me', auth, async (req, res) => {
 	try {
-		const user = await User.findByIdAndDelete(_id);
-		if (!user) {
-			return res.status(404).send();
-		}
-		res.send(user);
+		await req.user.remove();
+		res.send(req.user);
 	} catch (e) {
 		res.status(500).send(e);
 	}
