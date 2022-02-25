9b974a8da14054236bc56e0bd99a082bd2642ae0
BlueWorldInc <keamybakunin@gmail.com>
2022-02-25 18:04:36 +0100

    save

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -29,7 +29,7 @@ router.get('/tasks', auth, async (req, res) => {
 router.get('/tasks/:id', auth, async (req, res) => {
 	const _id = req.params.id;
 	try {
-		const task = await Task.findById(_id);
+		const task = await Task.findOne({ _id, owner: req.user._id });
 		if (!task) {
 			return res.status(404).send();
 		}
